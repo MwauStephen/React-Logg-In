40174ad66f1850f0e0fb6650d8b87f5dfb0593e9
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 06:42:59 +0800

    added event handlers on the inputs elements

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,10 +4,24 @@ import Card from "../UI/Card/Card";
 import styles from "./Login.module.css";
 
 const Login = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEmteredPassword] = useState("");
   const [passwordIsValid, setPassworIsValid] = useState();
 
-  const validateEmailHandler = () => {};
+  //   handling email and password inputs
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEmteredPassword(event.target.value);
+  };
+
+  //   validate email and password
+  const validateEmailHandler = () => {
+      
+  };
   const validatePasswordHandler = () => {};
 
   const formSubmitHandler = (event) => {
@@ -22,7 +36,12 @@ const Login = () => {
           }`}
         >
           <label htmlFor="email">E-mail:</label>
-          <input type="email" id="email" onBlur={validateEmailHandler} />
+          <input
+            type="email"
+            id="email"
+            onBlur={validateEmailHandler}
+            onChange={emailChangeHandler}
+          />
         </div>
         <div
           className={`${styles.control} ${
@@ -33,11 +52,12 @@ const Login = () => {
           <input
             type="password"
             id="password"
+            onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
         </div>
         <div className={styles.actions}>
-          <Button>Login</Button>
+          <Button type="submit">Login</Button>
         </div>
       </form>
     </Card>
