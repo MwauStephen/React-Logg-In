007dcb496250b8c1f7db06944ad29cb3c2aef109
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 06:04:25 +0800

    added basic structure of the navigation

--- src/Components/MainHeader/Navigation.js
+++ src/Components/MainHeader/Navigation.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Navigation = () => {
-  return;
+const Navigation = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li>Users</li>
+        <li>Admin</li>
+        <li>
+          <button>Log Out</button>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
