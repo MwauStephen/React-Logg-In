2ff2440cd3bb4bfd2f651ce35669bec57d081b43
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 05:18:33 +0800

    added basic form inputs

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <form>
+      <div>
+        <label>E-mail:</label>
+        <input type="email" />
+      </div>
+      <div>
+        <label>Password:</label>
+      </div>
+      <div>
+        <button></button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
