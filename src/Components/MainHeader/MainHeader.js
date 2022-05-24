f12dda039ba1be433297f36d3db7d95251561287
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 15:04:24 +0800

    added the navigation component in the main header component

--- src/Components/MainHeader/MainHeader.js
+++ src/Components/MainHeader/MainHeader.js
@@ -1,12 +1,12 @@
 import React from "react";
-import styles from "./MainHeader.module.css";
 import Navigation from "./Navigation";
+import styles from "./MainHeader.module.css";
 
-const MainHeader = () => {
+const MainHeader = (props) => {
   return (
     <header className={styles["main-header"]}>
       <h1>A login Page</h1>
-      <Navigation />
+      <Navigation isLoggedIn={props.isAuthenticated} />
     </header>
   );
 };
