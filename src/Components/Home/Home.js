fcf349e32b458903aa23429a12213257f7cc5240
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 14:30:25 +0800

    added a card and a heading element

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Card from "../UI/Card/Card";
 
-const Home = () => {};
+const Home = (props) => {
+  return (
+    <Card>
+      <h1>Welcome Back ! 🤓</h1>
+    </Card>
+  );
+};
 
 export default Home;
