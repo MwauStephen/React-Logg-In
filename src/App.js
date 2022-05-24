0c349493e9b0400385d99f2481535a421dd6e84c
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 15:28:23 +0800

    added a log out handler on the home component as a prop

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ import Home from "./Components/Home/Home";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // login handler after validation of email and password(through lifting state)
   const loginHandler = (email, password) => {
     setIsLoggedIn(true);
   };
 
+  // Log out handler
+  const logOutHandler = () => {
+    setIsLoggedIn(false);
+  };
   return (
     <Fragment>
       <MainHeader isAuthenticated={isLoggedIn} />
       <main>
         {!isLoggedIn && <Login onLoggedIn={loginHandler} />}
 
-        {isLoggedIn && <Home />}
+        {isLoggedIn && <Home onLogOut={logOutHandler} />}
       </main>
     </Fragment>
   );
